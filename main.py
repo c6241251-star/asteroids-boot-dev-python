6ee36f9efa294df84c2d4afb7766017d1dbd1ea8
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-06-27 20:51:49 +0100

    create asteroid functionality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+import sys
 
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger    import log_state
-from player    import Player
+from asteroid      import Asteroid
+from asteroidfield import AsteroidField
+from constants     import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger        import log_state, log_event
+from player        import Player
 
 def main():
     pygame.init()
@@ -10,13 +13,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt     = 0.0
 
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+    Player.containers        = (updatable, drawable)
+    Asteroid.containers      = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
     player   = Player(player_x, player_y)
+
+    asteroidField = AsteroidField()
 
     while True:
         log_state()
@@ -27,6 +36,11 @@ def main():
         screen.fill("black")
 
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for item in drawable:
             item.draw(screen)
 
