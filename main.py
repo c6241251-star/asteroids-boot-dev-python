133873b64e6cd5adcd75a9579486da475b28d9fe
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-06-27 20:02:38 +0100

    add player movement functionality

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt     = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
-    player = Player(player_x, player_y)
+    player   = Player(player_x, player_y)
 
     while True:
         log_state()
@@ -22,8 +26,9 @@ def main():
             
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
